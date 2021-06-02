14faf9338b1766a66d21dffba28e88d8927c0058
Snebjorn <eskildd@gmail.com>
2021-06-02 16:43:18 +0200

    feat(eh02): added setup script for yig (ancient one)

---
+++ src/02 Forsaken Lore/Scripts/yig.js
@@ -0,0 +1,24 @@
+const { refCard, world } = require("@tabletop-playground/api");
+
+/** @type AncientOne */
+const yig = {
+  name: "Yig",
+  doom: 10,
+  sheetId: refCard.getId(),
+  mysteryTemplateIds: ["D3AAA13B4ABE379B1898ED88BC157857"],
+  researchTemplateIds: ["060A47CC4476B75FEA03B2BA3A8B9513"],
+  specialTemplateIds: {
+    "K'n-Yan Unearthed": ["4940BFCD4BD047688B26B894983C82CB"],
+  },
+  mythosDeck: {
+    stage1: { green: 1, yellow: 2, blue: 1 },
+    stage2: { green: 2, yellow: 3, blue: 1 },
+    stage3: { green: 2, yellow: 4, blue: 0 },
+  },
+  monsters: { Cultist: 6, "Serpent People": 1 },
+};
+
+if (!world.__eldritchHorror.alreadyLoaded.includes(refCard.getTemplateId())) {
+  world.__eldritchHorror.ancientOnes.push(yig);
+  world.__eldritchHorror.alreadyLoaded.push(refCard.getTemplateId());
+}
