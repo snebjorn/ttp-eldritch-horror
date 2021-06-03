b7e96f5d682b17503e2185ba2ee8f87f178e388e
Snebjorn <eskildd@gmail.com>
2021-06-03 19:12:49 +0200

    feat: changing state using the number keys will now show health 5 if 5 was pressed

---
+++ src/01 Core/Scripts/health-sanity.js
@@ -0,0 +1,7 @@
+const { refMultistate } = require("@tabletop-playground/api");
+
+// aligns the number pressed with the number shown on the token
+// without this, pressing 2 would show 3 on the token because states are 0 indexed
+refMultistate.onNumberAction.add((healthOrSanity, player, number) => {
+  healthOrSanity.setState(number - 1);
+});
