c423dd2d270ff29015b5b4ab21258650e70b6f4c
Snebjorn <eskildd@gmail.com>
2021-06-07 22:11:10 +0200

    feat(eh01): infinite tokens for improve/ticket/rumor/eldritch/mystery

--- src/01 Core/Scripts/global.js
+++ src/01 Core/Scripts/global.js
@@ -9,14 +9,7 @@ world.broadcastChatMessage(`
 ##  Eldritch  Horror  ##
 #################
 
-To get started select the desired expansion(s), difficulty and ancient one in the UI.
-
-###############
-## Using tokens ##
-###############
-
-When you need to use the tokens (improvement/ticket/rumor/eldritch/mystery) don't pick them up. Simply copy and paste it (CTRL+C/CTRL+V)
-Delete your copy when you no longer need it.
+To get started select the desired expansion(s), difficulty and Ancient One in the UI.
 `);
 
 function initGlobalObject() {
