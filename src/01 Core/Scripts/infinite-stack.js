89cef1f10b38fee2eceae9a464ce1b3ed16ae291
Snebjorn <eskildd@gmail.com>
2021-07-13 23:40:11 +0200

    fixes(eh01): infinite tokens works again with the latest TTP version

--- src/01 Core/Scripts/infinite-stack.js
+++ src/01 Core/Scripts/infinite-stack.js
@@ -11,8 +11,15 @@ refCard.onRemoved.add((stack, card) => {
 });
 
 // on card inserted remove it from the stack and destroy it
-refCard.onInserted.add((stack, _card, offset) => {
+refCard.onInserted.add((stack, _card, offset, player) => {
+  if (player === undefined) {
+    return; // abort when a script is inserting
+  }
+
   // card.destroy() doesn't work, so have to take() it from the stack first
   const insertedCard = stack.takeCards(1, true, offset);
-  insertedCard.destroy();
+
+  if (insertedCard) {
+    insertedCard.destroy();
+  }
 });
